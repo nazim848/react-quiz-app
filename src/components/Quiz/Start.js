abf29f1c4b47a4a34027a3706f1288d79344b394
nazim848 <28640245+nazim848@users.noreply.github.com>
2022-12-31 18:34:39 +0530

    some changes

--- src/components/Quiz/Start.js
+++ src/components/Quiz/Start.js
@@ -4,7 +4,7 @@ import classes from '../../styles/Quiz/Start.module.css'
 const Start = (props) => {
 	return (
 		<div className={classes['start-quiz']}>
-			<h1 className={classes.title}>Ready to start React quiz??</h1>
+			<h1 className={classes.title}>Ready to start React quiz?</h1>
 			<Button onClick={props.onClick}>Yes, Start Quiz!</Button>
 		</div>
 	)
